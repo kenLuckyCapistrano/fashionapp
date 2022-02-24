72c4af15da06fadc313e8a88d8f61dcf6fab4de5
kenLuckyCapistrano <ken.lucky.capistrano@accenture.com>
2022-02-24 22:14:55 +0800

    implement Camel Case on csv header

--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get, Post, UploadedFile, UseInterceptors, UnsupportedMediaTypeException } from '@nestjs/common';
+import { Controller, Get, Post, UseInterceptors, UnsupportedMediaTypeException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { Express } from 'express';
-import { createReadStream, readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import { diskStorage } from 'multer';
-import { join } from 'path';
 import { SalesService } from './sales.service';
 import { parse } from 'papaparse';
-
+import camelcase
+ from 'camelcase';
 @Controller('sales')
 export class SalesController {
     constructor (private salesService: SalesService){}
+
     @Post('/record')
     @UseInterceptors(
         FileInterceptor('file', { storage: diskStorage({
@@ -25,14 +25,15 @@ export class SalesController {
         const parsedData = await  parse(data, {
             header:true,
             skipEmptyLines:true,
-            transformHeader: (header) => header.toLowerCase().replace('#','').trim(),
+            transformHeader: (header) => camelcase(header.toLowerCase().replace('#','').trim()),
             complete: (results) => results.data,
         });
+        console.log(parsedData);
         if (!parsedData.data){
             throw new Error('No data to process');
         }
         for (const element of parsedData.data) {
-            await this.salesService.create(element['user_name'], parseInt(element['age']), parseFloat(element['height']), element['gender'], parseFloat(element['sales']),element['last_purchase_date']);
+            await this.salesService.create(element['userName'], parseInt(element['age']), parseFloat(element['height']), element['gender'], parseFloat(element['sales']),element['lastPurchaseDate']);
         }
         
         
