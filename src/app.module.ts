b18e449d40952a42a08717d3c0b6d57ac9e35dc4
kenLuckyCapistrano <ken.lucky.capistrano@accenture.com>
2022-02-24 10:28:43 +0800

    Create Sales Module, Controller and Service Files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
