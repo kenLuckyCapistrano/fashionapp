40d33bb36e5b9d5e0f44ee9a74ff70a05bd02aa2
kenLuckyCapistrano <ken.lucky.capistrano@accenture.com>
2022-02-24 11:32:05 +0800

    Setup Database and Entity Connection for Sales Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [SalesModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'db.sqlite',
+    entities: [],
+    synchronize: true
+  }),
+   SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SalesController } from './sales.controller';
 import { SalesService } from './sales.service';
+import { Sales } from './sales.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Sales])],
   controllers: [SalesController],
   providers: [SalesService]
 })
