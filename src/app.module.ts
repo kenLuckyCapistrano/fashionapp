17e79d25715a85b04f6ec9f1569806c062b62c4e
kenLuckyCapistrano <ken.lucky.capistrano@accenture.com>
2022-02-28 07:28:30 +0800

    adding .env files for dev and test environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { SalesModule } from './sales/sales.module';
 import { Sales } from './sales/sales.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [Sales],
-    synchronize: true
-  }),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [Sales],
+        };
+      }
+    }),
    SalesModule],
   controllers: [AppController],
   providers: [AppService],
